1a40b5a5fd01d07e6cdcd6e196ae2857330ba0e1
Daniel Sângelo <danielsangelo@hotmail.com>
2025-11-28 11:56:30 -0300

    feat: add index rout for email list and create components

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'post' => null,
+    'put' => null,
+    'delete' => null,
+    'patch' => null,
+])
+
+@php
+    $httpMethod = $post || $put || $delete || $patch ? 'post' : 'get';
+@endphp
+
+<form method="{{ $httpMethod }}" {{ $attributes->class(['gap-4 flex flex-col']) }}>
+    @csrf
+
+    @if ($put)
+        @method('put')
+    @endif
+
+    @if ($delete)
+        @method('delete')
+    @endif
+
+    @if ($patch)
+        @method('patch')
+    @endif
+
+    {{  $slot }}
+</form>
