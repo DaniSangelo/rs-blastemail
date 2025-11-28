fe7d1dda3a35419de59c8bf07d7cab51c82cfeb7
Daniel Sângelo <danielsangelo@hotmail.com>
2025-11-28 16:45:48 -0300

    feat: add table and td components

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -6,14 +6,25 @@
     </x-slot>
 
     <x-card>
-        @forelse ($emailLists as $list)
-            something here
-        @empty
+        @unless ($emailLists->isEmpty())
+            <x-table :headers="['#', __('Email list'), __('Qty'), __('Actions')]">
+                <x-slot name="body">
+                    @foreach ($emailLists as $list)
+                        <tr>
+                            <x-table.td>{{ $list->id }}</x-td>
+                            <x-table.td>{{ $list->title }}</x-td>
+                            <x-table.td>{{ $list->subscribers->count() }}</x-td>
+                            <x-table.td>Silver</x-td>
+                        </tr>
+                    @endforeach
+                </x-slot>
+            </x-table>
+        @else
             <div class="flex justify-center">
                 <x-link-button :href="route('email-list.create')">
                     {{ __('Create New Email List') }}
                 </x-link-button>
             </div>
-        @endforelse 
+        @endunless
     </x-card>
 </x-layouts.app>
