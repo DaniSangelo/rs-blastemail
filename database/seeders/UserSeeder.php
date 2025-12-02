aee51d73e467f14cf048a19a591b9a597475c10b
Daniel Sângelo <danielsangelo@hotmail.com>
2025-12-01 23:27:06 -0300

    feat: add and refactor seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\EmailTemplate;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class EmailTemplateSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        EmailTemplate::factory()->count(10)->create();
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
