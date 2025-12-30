94a34fc92bfd21ac3b7bf047287e85e95a7274af
Daniel Sângelo <danielsangelo@hotmail.com>
2025-12-30 16:39:32 -0300

    fix: set middlewares to routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/t/{mail}/c', [TrackingController::class, 'clicks'])->name('tracking
 
 Route::redirect('/dashboard', '/campaign')->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware([])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
