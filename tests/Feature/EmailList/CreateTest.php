ea478b0a0ab6eca0c2de65d0d570c0bd19927665
Daniel Sângelo <danielsangelo@hotmail.com>
2025-12-30 15:04:34 -0300

    test: refactor to use setUp function

--- tests/Feature/EmailList/CreateTest.php
+++ tests/Feature/EmailList/CreateTest.php
@@ -2,17 +2,20 @@
 
 namespace Tests\Feature\EmailList;
 
-use App\Models\User;
 use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class CreateTest extends TestCase
 {
+    protected function setUp():void
+    {
+        parent::setUp();
+        $this->login();
+    }
+
     public function test_it_should_be_able_to_create_an_email_list()
     {
         // $this->withoutExceptionHandling();
-        $this->login();
-
         $data = [
             'title' => 'Email list test',
             'listFile' => UploadedFile::fake()->createWithContent(
@@ -40,22 +43,18 @@ class CreateTest extends TestCase
 
     public function test_title_should_be_required()
     {
-        $this->login();
-
         $this->post(route('email-list.create'), [])
             ->assertSessionHasErrors(['title' => 'The title field is required.']);
     }
 
      public function test_title_should_have_a_max_of_255_characters()
     {
-        $this->login();
         $this->post(route('email-list.create'), ['title' => str_repeat('*', 256)])
             ->assertSessionHasErrors(['title' => 'The title field must not be greater than 255 characters.']);
     }
 
     public function test_file_should_be_required()
     {
-        $this->login();
         $this->post(route('email-list.create'), [])
             ->assertSessionHasErrors(['listFile' => 'The list file field is required.']);
     }
